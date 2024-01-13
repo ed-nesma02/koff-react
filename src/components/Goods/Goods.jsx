95d433337ce9ad64b7225c07621f1d5fcededc34
ed-nesma02 <shkurko26081978@gmail.com>
2024-01-13 18:26:31 +0200

    lesson 2 fix

--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -1,12 +1,13 @@
 import { Container } from "../../views/Container/Container";
 import { CardItem } from "../CardItem/CardItem";
 import s from "./Goods.module.scss";
+import cn from "classnames";
 
 export const Goods = () => {
   return (
     <section className={s.goods}>
       <Container>
-        <h2 className={s.title}>Список товаров</h2>
+        <h2 className={cn(s.title, "visually-hidden")}>Список товаров</h2>
         <ul className={s.list}>
           <li className={s.item}>
             <CardItem />
